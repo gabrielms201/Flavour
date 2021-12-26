4e364765ac2095e72ec29c623d5e1239c9183af5
Ricardo Ruiz <ricardogabrielmsruiz@gmail.com>
2021-12-26 13:55:50 -0300

    Change SweetTaste Entry point heatbeat

--- Flavour/Core/Application.cpp
+++ Flavour/Core/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 #include <iostream>
+#include <chrono>
+#include <thread>
+
 namespace Flavour 
 {
 	Application::Application()
@@ -9,12 +12,19 @@ namespace Flavour
 	Application::~Application()
 	{
 	}
+    
 	void Application::Run()
 	{
-		std::cout << "Up and running \n";
+        std::cout << "Flavour Engine Speaking" <<  std::endl << "Hello World!" << std::endl  << std::endl;
 		while (true)
 		{
-
+            for (int i = 0; i <= 10; i++)
+            {
+                std::cout << "Up and running  - " <<   i  << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+            std::cout << std::endl;
 		}
 	}
 }
